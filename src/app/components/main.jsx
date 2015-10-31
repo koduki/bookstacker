1859b0f1a212d4684d15ece08360103ea678600b
koduki <pascalm3-git@gmail.com>
2015-11-01 08:28:13 +0900

    to class syntax.

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,37 +1,38 @@
 /** In this file, we create a React component which incorporates components provided by material-ui */
 
-const React = require('react');
-const RaisedButton = require('material-ui/lib/raised-button');
-const Dialog = require('material-ui/lib/dialog');
-const ThemeManager = require('material-ui/lib/styles/theme-manager');
-const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
-const Colors = require('material-ui/lib/styles/colors');
+import React from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Dialog from 'material-ui/lib/dialog';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import Colors from 'material-ui/lib/styles/colors';
 
-const Main = React.createClass({
-
-  childContextTypes: {
+class Main extends React.Component {
+  static childContextTypes = {
     muiTheme: React.PropTypes.object,
-  },
+  };
 
-  getInitialState () {
-    return {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
     };
-  },
+  }
 
   getChildContext() {
     return {
       muiTheme: this.state.muiTheme,
     };
-  },
+  }
 
   componentWillMount() {
     let newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
       accent1Color: Colors.deepOrange500,
     });
-    
+
     this.setState({muiTheme: newMuiTheme});
-  },
+  }
 
   render() {
 
@@ -56,16 +57,16 @@ const Main = React.createClass({
         <h1>material-ui</h1>
         <h2>example project</h2>
 
-        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
+        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap.bind(this)} />
 
       </div>
     );
-  },
+  }
 
   _handleTouchTap() {
     this.refs.superSecretPasswordDialog.show();
-  },
+  }
 
-});
+};
 
 module.exports = Main;
